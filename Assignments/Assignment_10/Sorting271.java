457b706ebb2a94671d2b5b7a8164aba72d83d0b7
Arjun Parihar <151101600+arjunparihar11@users.noreply.github.com>
2024-08-05 20:09:07 +0000

    Sort Method

--- Assignments/Assignment_10/Sorting271.java
+++ Assignments/Assignment_10/Sorting271.java
@@ -1,4 +1,5 @@
 public class Sorting271 {
+
     /**
      * Merge two arrays into a single sorted array
      * @param left First Sorted Array
@@ -28,12 +29,13 @@ public class Sorting271 {
             iL++;
         }
         while (iR < right.length) {//if remaining elements exist
-            merged[i] = left[iR];//add to end of merge
+            merged[i] = right[iR];//add to end of merge
             i++;
             iR++;
         }
         return merged;
     }
+
     /**
      * String to print out array in testing
      * @param x array being printed
@@ -52,14 +54,41 @@ public class Sorting271 {
         return sb.toString();//return completed string
     }
 
+    public static int[] sort(int[] array) {
+        int[] sorted = new int[array.length];//sorted array to return
+        if(array.length <= 1) {//if array has 1 or less elements
+            sorted = array;//then it is already sorted
+        } else {//else we need to sort it via merge sort
+            int mid = array.length / 2; // find the midpoint
+            int[] arr1 = new int[mid];//first hald array
+            int[] arr2 = new int[array.length - mid];//second half array
+            for (int i = 0; i < mid; i++) {//fill first half
+                arr1[i] = array[i];
+            }
+            for (int i = mid; i < array.length; i++) {//fill second half
+                arr2[i - mid] = array[i];
+            }
+            //System.out.printf("%nArr1: %s", printArray(arr1));
+            //System.out.printf("%nArr2: %s", printArray(arr2));
+            arr1 = sort(arr1);//recursive sort till each array has 1 element
+            arr2 = sort(arr2);//recursive sort till each array has 1 element
+            sorted = merge(arr1, arr2);//merge together into a sorted array
+        }
+        //System.out.printf("%nSorted: %s", printArray(sorted));
+        return sorted;//return sorted array
+    }
+
     public static void main(String[] args) {
         //TEST MERGE
         int[] a = {1,2,8};
         int[] b = {0,5,6,7,9};
         int[] c = merge(a,b);
-        System.out.printf("%nArray A:%n%s%n",printArray(a));
-        System.out.printf("%nArray B:%n%s%n",printArray(b));
+        System.out.printf("%nArray A:%n%s",printArray(a));
+        System.out.printf("%nArray B:%n%s",printArray(b));
         System.out.printf("%nMerge(A,B):%n%s%n",printArray(c));
-
+        //TEST SORT
+        int[] d = {10, 8, 5, 3, 7, 6, 2, 1};
+        int[] e = sort(d);
+        System.out.printf("%nOriginal Array:%n%s%nSorted Array:%n%s%n",printArray(d), printArray(e));
     }
 }
